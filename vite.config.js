6781eec2b78c2c443326bb097207222457afecc5
Avishek255874 <avisheksharma6290@gmail.com>
2023-10-25 22:58:06 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { VitePWA } from "vite-plugin-pwa";
+const manifestForPlugIn = {
+  registerType:'prompt',
+  includeAssests:["vite.svg"],
+  manifest:{
+    name:"React-vite-app",
+    short_name:"react-vite-app",
+    description:"I am a simple vite app",
+  theme_color:'#171717',
+  background_color:'#f0e7db',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  }
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
 })
