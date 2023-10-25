94b04da998591f3e9eedd5792b4aee1eb9fef09f
Avishek255874 <avisheksharma6290@gmail.com>
2023-10-26 00:06:23 +0530

    serviceWorker2

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,23 +1,24 @@
-
-// src/serviceWorker.ts
-
 self.addEventListener('install', (event) => {
-                    event.waitUntil(
-                      caches.open('my-cache').then((cache) => {
-                        return cache.addAll([
-                          '/',
-                          '/index.html',
-                          '/app.js',
-                          // Add more URLs for assets to cache
-                        ]);
-                      })
-                    );
-                  });
-                  
-                  self.addEventListener('fetch', (event) => {
-                    event.respondWith(
-                      caches.match(event.request).then((response) => {
-                        return response || fetch(event.request);
-                      })
-                    );
-                  })
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/app.js',
+        // Add more URLs for assets to cache
+      ]).catch((error) => {
+        console.error('Cache addAll failed:', error);
+      });
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch((error) => {
+        console.error('Fetch failed:', error);
+      });
+    })
+  );
+});
