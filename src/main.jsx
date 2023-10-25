8734bfaea8e9837450c0aba32e35950d35a6c258
Avishek255874 <avisheksharma6290@gmail.com>
2023-10-25 23:49:48 +0530

    serviceWorker

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+if ('serviceWorker' in navigator) {
+                    navigator.serviceWorker.register('../serviceWorker.js')
+                      .then((registration) => {
+                        console.log('Service Worker registered with scope:', registration.scope);
+                      })
+                      .catch((error) => {
+                        console.error('Service Worker registration failed:', error);
+                      });
+                  }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from "vite-plugin-pwa";
-const manifestForPlugIn = {
-  registerType:'prompt',
-  includeAssests:["vite.svg"],
-  manifest:{
-    name:"React-vite-app",
-    short_name:"react-vite-app",
-    description:"I am a simple vite app",
-  theme_color:'#171717',
-  background_color:'#f0e7db',
-  display:"standalone",
-  scope:'/',
-  start_url:"/",
-  orientation:'portrait'
-  }
-}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(manifestForPlugIn)],
+  plugins: [react() ],
 })
