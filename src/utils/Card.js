f98ee1f45249424bb5dcdf7fd6a83d02d7605654
Avishek255874 <avisheksharma6290@gmail.com>
2023-10-26 13:31:02 +0530

    service

--- src/utils/Card.js
+++ src/utils/Card.js
@@ -6,7 +6,7 @@ export const Cards = [
     middletext: "accounting",
     buttomtext: "material",
     Myphoto:
-      "https://scontent.fccu2-1.fna.fbcdn.net/v/t39.30808-6/363374086_1982649622107457_5607490186300255942_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=whjVkRpAkmgAX8F5pIX&_nc_ht=scontent.fccu2-1.fna&oh=00_AfDN6rATZcLX6ItJOE0zZYpFhYIQzO3-v-1U5bo7hOLCug&oe=653711B9",
+      "https://scontent.fccu2-1.fna.fbcdn.net/v/t39.30808-6/363374086_1982649622107457_5607490186300255942_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=DLsBzlkOHFYAX8Ofsaj&_nc_ht=scontent.fccu2-1.fna&oh=00_AfDFmsTWnGIpkWy7AWbld8noBpPoYN-bnWJ0EGlIPKpYOA&oe=653EFAB9",
     BgcoloR: "bg-blue-100",
     TextColor:"text-blue-500",
     RoundColor:"bg-blue-200",
@@ -14,14 +14,14 @@ export const Cards = [
   },
   
   {
-    circletext: "Read",
-    Name: "Avishek Sharma",
-    Uppertext: "Summary of",
-    middletext: "accounting",
+    circletext: "Post",
+    Name: "Amit Sharma",
+    Uppertext: "Historical",
+    middletext: "presentation",
     buttomtext: "material",
     Myphoto:
-      "https://scontent.fccu2-1.fna.fbcdn.net/v/t39.30808-6/363374086_1982649622107457_5607490186300255942_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=whjVkRpAkmgAX8F5pIX&_nc_ht=scontent.fccu2-1.fna&oh=00_AfDN6rATZcLX6ItJOE0zZYpFhYIQzO3-v-1U5bo7hOLCug&oe=653711B9",
-    BgcoloR: "bg-blue-100",
+      "https://scontent.fccu2-1.fna.fbcdn.net/v/t39.30808-6/394401067_3494090857506484_4519767327583687618_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=_qSZhhFPhh8AX-ap8a4&_nc_ht=scontent.fccu2-1.fna&oh=00_AfCzKA6Txw2GusA_KmXSTw8vivIYv2mID_9KeKzKGVW7Ng&oe=653EABF9",
+    BgcoloR: "bg-green-100",
     TextColor:"text-blue-500",
     RoundColor:"bg-blue-200",
     Created:"Created By"
