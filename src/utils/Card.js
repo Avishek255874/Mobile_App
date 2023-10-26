df4474e00261be7906ff7ef8d5e46fd884875c27
Avishek255874 <avisheksharma6290@gmail.com>
2023-10-26 13:21:14 +0530

    serviceWorker2

--- src/utils/Card.js
+++ src/utils/Card.js
@@ -27,18 +27,6 @@ export const Cards = [
     Created:"Created By"
   },
   
-  {
-    circletext: "Read",
-    Name: "Avishek Sharma",
-    Uppertext: "Summary of",
-    middletext: "accounting",
-    buttomtext: "material",
-    Myphoto:
-      "https://scontent.fccu2-1.fna.fbcdn.net/v/t39.30808-6/363374086_1982649622107457_5607490186300255942_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=whjVkRpAkmgAX8F5pIX&_nc_ht=scontent.fccu2-1.fna&oh=00_AfDN6rATZcLX6ItJOE0zZYpFhYIQzO3-v-1U5bo7hOLCug&oe=653711B9",
-    BgcoloR: "bg-blue-100",
-    TextColor:"text-blue-500",
-    RoundColor:"bg-blue-200",
-    Created:"Created By"
-  },
+ 
   
 ];
